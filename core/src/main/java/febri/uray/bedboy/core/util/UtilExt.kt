42ab6652f52344888d4a114951772e8ed9c30170
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2024-12-31 22:09:17 +0700

    add: unit test news ext

--- core/src/main/java/febri/uray/bedboy/core/util/UtilExt.kt
+++ core/src/main/java/febri/uray/bedboy/core/util/UtilExt.kt
@@ -1,16 +1,21 @@
 package febri.uray.bedboy.core.util
 
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 inline fun <T> StateFlow<T>.collectWhenStarted(
     fragmentActivity: FragmentActivity,
     crossinline onCollect: (T) -> Unit
 ) {
-    fragmentActivity.lifecycleScope.launchWhenStarted {
-        this@collectWhenStarted.collect {
-            onCollect(it)
+    fragmentActivity.lifecycleScope.launch {
+        fragmentActivity.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            this@collectWhenStarted.collect {
+                onCollect(it)
+            }
         }
     }
 }
