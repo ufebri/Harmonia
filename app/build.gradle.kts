d9970e303654c6cacb80cfe22414ca7c0b5b9b86
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2025-01-02 01:27:24 +0700

    add: news mapper test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(apikeyProperties["KEYSTORE_FILE"] as String)
+            storeFile = file(rootProject.file("app/harmonia.jks"))
             storePassword = apikeyProperties.getProperty("KEYSTORE_PASSWORD")
             keyAlias = apikeyProperties.getProperty("KEYSTORE_ALIAS")
             keyPassword = apikeyProperties.getProperty("KEY_PASSWORD")
